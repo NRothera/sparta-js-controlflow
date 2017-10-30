fba4c95c8c4c870909c8ca937ee3df7a16f0dca5
NRothera <31472463+NRothera@users.noreply.github.com>
2017-10-30 16:04:21 +0000

    practice done

--- main.js
+++ main.js
@@ -9,14 +9,48 @@
 // }
 
 
-var moneyInPocket = prompt('How much money do you have?');
-var drinksHad = prompt('How many drinks have you had?')
-var freeCrips = prompt('How much are crips?');
+// var moneyInPocket = prompt('How much money do you have?');
+// var drinksHad = prompt('How many drinks have you had?')
+// var freeCrips = prompt('How much are crips?');
+//
+//  if (moneyInPocket > 10 &&  drinksHad < 3) {
+//   console.log("I will buy another drink");
+// } else if (moneyInPocket > 5 || freeCrips === 0){
+//   console.log("I'll buy some crisps");
+// }else{
+//   console.log('Off home I go');
+// }
 
- if (moneyInPocket > 10 &&  drinksHad < 3) {
-  console.log("I will buy another drink");
-} else if (moneyInPocket > 5 || freeCrips === 0){
-  console.log("I'll buy some crisps");
-}else{
-  console.log('Off home I go');
-}
+// var name ="thing";
+//
+// if (name === "Bob"){
+//   console.log("Hi Bob");
+// }else if(name === "Tim"){
+//   console.log('Hi Tim')
+// }
+//
+// switch (name) {
+//   case 'Tim':
+//     console.log('Hi Tim')
+//     break;
+//   case "Bob":
+//     console.log("Hi Bob")
+//     break;
+//   default:
+//   console.log('Hello nameless blob')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
